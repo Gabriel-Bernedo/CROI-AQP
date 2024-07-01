19c337d80b109c47f48d002e402741e5ffff26ff
Gabriel-Bernedo <132683380+Gabriel-Bernedo@users.noreply.github.com>
2024-07-01 11:09:25 -0500

    Modularizar lógica

--- F_MATLAB/code/C_Electrodomestico.m
+++ F_MATLAB/code/C_Electrodomestico.m
@@ -7,15 +7,31 @@ classdef C_Electrodomestico
       tipo
    end
    methods
-      function obj = C_Producto(nombre, potencia, horas_dia, dia_semana, tipo)
-         obj.nombre = nombre;
-         obj.potencia = potencia;
-         obj.horas_dia = horas_dia;
-         obj.dia_semana = dia_semana;
-         obj.tipo = tipo;
+      function obj = C_Electrodomestico(nombre, potencia, horas_dia, dia_semana, tipo)
+         if dia_semana > 7 || dia_semana < 1
+            ME = MException('C_Producto:Invalid_Param', 'Ingrese un número de días válido');
+            throw(ME);
+         elseif horas_dia > 24 || horas_dia < 1
+            ME = MException('C_Producto:Invalid_Param', 'Ingrese un número de horas válido');
+            throw(ME);
+         elseif potencia < 1
+            ME = MException('C_Producto:Invalid_Param', 'Ingrese un consumo valido (> 0)');
+            throw(ME);
+         else
+            obj.nombre = nombre;
+            obj.potencia = potencia;
+            obj.horas_dia = horas_dia;
+            obj.dia_semana = dia_semana;
+            obj.tipo = tipo;
+         end
       end
       function x = ConsumoTotal(obj, mes)
          x = obj.potencia * obj.horas_dia * obj.dia_semana * mes;
+         if obj.tipo == "Esencial"
+               x = x * .80;
+         elseif obj.tipo == "No Esencial"
+               x = x * .60;
+         end
       end
    end
 end
