156eb358cfe7ccd8f2a7d68a8b1712065bdaccf6
Gabriel-Bernedo <132683380+Gabriel-Bernedo@users.noreply.github.com>
2024-07-16 00:19:56 -0500

    XD, fixing, data breaches :P

--- F_MATLAB/code/conectarBD.m
+++ F_MATLAB/code/conectarBD.m
@@ -1,10 +1,10 @@
 function conn = conectarBD()
     dbname = 'croi_aqp';
     username = 'root';
-    password = 'admin123';
+    password = 'Gabokiller0711';
     driver = 'com.mysql.cj.jdbc.Driver';
-    url = 'jdbc:mysql://localhost:3306/electrodomesticos_db';
-    conn = database(dbname, username, password, driver, url);
+    url = 'jdbc:mysql://localhost:3306';
+    conn = database(dbname, username, password);
     if ~isopen(conn)
         error('No se pudo conectar a la base de datos: %s', conn.Message);
     end
--- F_MATLAB/code/startup.m
+++ F_MATLAB/code/startup.m
@@ -1,1 +1,2 @@
-javaaddpath('C:\Users\Jeremy\Downloads\mysql-connector-j-9.0.0\mysql-connector-j-9.0.0\mysql-connector-j-9.0.0.jar');
+javaaddpath('C:\Users\ASUS\Downloads\mysql-connector-j-9.0.0\mysql-connector-j-9.0.0\mysql-connector-j-9.0.0.jar');
+fprintf("Java añadido\n");
