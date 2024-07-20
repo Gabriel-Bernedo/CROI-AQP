22aceb22167765c7cd11f6373f0a18ee0175fa55
Jeremy <132602883+jperezh04@users.noreply.github.com>
2024-07-20 12:14:43 -0500

    Nueva app

--- F_MATLAB/code/conectarBD.m
+++ F_MATLAB/code/conectarBD.m
@@ -1,7 +1,7 @@
 function conn = conectarBD()
     dbname = 'croi_aqp';
     username = 'root';
-    password = 'Gabokiller0711';
+    password = 'admin123';
     driver = 'com.mysql.cj.jdbc.Driver';
     url = 'jdbc:mysql://localhost:3306';
     conn = database(dbname, username, password);
