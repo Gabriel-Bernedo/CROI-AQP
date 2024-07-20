c3bf48a6350d95e39785715340b883e8df54d593
Jeremy <132602883+jperezh04@users.noreply.github.com>
2024-07-20 18:46:19 -0500

    Interfaz lista, falta lógica Mod 1

--- F_MATLAB/code/FC_showView.m
+++ F_MATLAB/code/FC_showView.m
@@ -11,7 +11,7 @@ function x = FC_showView(app, view)
         app.P_TablaRecibo_Consumo_1.Visible = 'off';
         app.P_Anadir_IngresarE_1.Visible = 'off';
         app.P_Botones_Recibo_1.Visible = 'off';
-
+        app.P_Botones_CalculadoraConsumo_1.Visible = "off";
     case "1.1" % Click ingresar recibo
         app.P_inicio_1.Visible = 'off';
         app.P_IngresarRecibo_1.Visible = 'on';
@@ -33,27 +33,28 @@ function x = FC_showView(app, view)
         app.P_TablaRecibo_Consumo_1.Visible = 'on';
         
     case "1.2.1" % Click en añadir en Ingresar electrodomésticos
-        app.P_Inicio_1.Visible = 'off';
+        app.P_inicio_1.Visible = 'off';
         app.P_Anadir_IngresarE_1.Visible = 'on';
-        app.P_Gestionar_IngresarE.Visible = 'off';
+        app.P_Gestionar_IngresarE_1.Visible = 'off';
         app.P_CalcularConsumo_IngresarE_1.Visible = 'off';
         app.P_ModificarElectrodomesticos_IngresarE_1.Visible = "off";
-        
+        app.P_Botones_CalculadoraConsumo_1.Visible = "on";
+
     case "1.2.2" % Click en gestionar en Ingresar electrodomesticos
         app.P_Anadir_IngresarE_1.Visible = "off";
-        app.P_Gestionar_IngresarE.Visible = "on";
+        app.P_Gestionar_IngresarE_1.Visible = "on";
         app.P_CalcularConsumo_IngresarE_1.Visible = "off";
         app.P_ModificarElectrodomesticos_IngresarE_1.Visible = "off";
         
     case "1.2.3" % Click en calcularConsumo en Ingresar electrodomesticos
         app.P_Anadir_IngresarE_1.Visible = "off";
-        app.P_Gestionar_IngresarE.Visible = "off";
+        app.P_Gestionar_IngresarE_1.Visible = "off";
         app.P_CalcularConsumo_IngresarE_1.Visible = "on";
         app.P_ModificarElectrodomesticos_IngresarE_1.Visible = "off";
         
     case "1.2.4" % Click en ModificarElectrodomésticos en Ingresar electrodomesticos
         app.P_Anadir_IngresarE_1.Visible = "off";
-        app.P_Gestionar_IngresarE.Visible = "off";
+        app.P_Gestionar_IngresarE_1.Visible = "off";
         app.P_CalcularConsumo_IngresarE_1.Visible = "off";
         app.P_ModificarElectrodomesticos_IngresarE_1.Visible = "on";
         
