e901e9d81b43af31ecb49d7c3ca9235c4d1abb2e
Read-Scoorp <132708685+WillianAyamamani@users.noreply.github.com>
2024-07-23 15:16:59 -0500

    Update conectarBD.m

--- F_MATLAB/code/bd/conectarBD.m
+++ F_MATLAB/code/bd/conectarBD.m
@@ -1,11 +1,7 @@
 function conn = conectarBD()
     dbname = 'ConectarBD';
     username = 'root';
-<<<<<<< HEAD
-    password = '';
-=======
     password = 'Gabokiller0711';
->>>>>>> 2a7f3b2396628ba74924213d2d2f55085f4da62f
     driver = 'com.mysql.cj.jdbc.Driver';
     url = 'jdbc:mysql://localhost:3306';
     conn = database(dbname, username, password);
