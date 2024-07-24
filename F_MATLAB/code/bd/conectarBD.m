546b5384c61aa1d6e7d720f08591a035d4984fc3
Read-Scoorp <132708685+WillianAyamamani@users.noreply.github.com>
2024-07-23 20:22:23 -0500

    Add files via upload

---
+++ F_MATLAB/code/bd/conectarBD.m
@@ -0,0 +1,14 @@
+function conn = conectarBD()
+    %nombre de tu conexion
+    dbname = '';
+    %Usuario de tu conexion
+    username = '';
+    %password de tu conexion
+    password = '';
+    driver = 'com.mysql.cj.jdbc.Driver';
+    url = 'jdbc:mysql://localhost:3306';
+    conn = database(dbname, username, password);
+    if ~isopen(conn)
+        error('No se pudo conectar a la base de datos: %s', conn.Message);
+    end
+end
