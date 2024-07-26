3a19a729ed355c06ffd3ce2c05b3e9f815365512
Francis <fayamamani@unsa.edu.pe>
2024-07-26 00:48:19 -0500

    BD

--- F_MATLAB/code/bd/conectarBD.m
+++ F_MATLAB/code/bd/conectarBD.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function conn = conectarBD()
     %nombre de tu conexion
     dbname = 'ConectarBD';
@@ -13,19 +12,4 @@ function conn = conectarBD()
         error('No se pudo conectar a la base de datos: %s', conn.Message);
     end
 end
-=======
-function conn = conectarBD()
-    %nombre de tu conexion
-    dbname = '';
-    %Usuario de tu conexion
-    username = '';
-    %password de tu conexion
-    password = '';
-    driver = 'com.mysql.cj.jdbc.Driver';
-    url = 'jdbc:mysql://localhost:3306';
-    conn = database(dbname, username, password);
-    if ~isopen(conn)
-        error('No se pudo conectar a la base de datos: %s', conn.Message);
-    end
-end
->>>>>>> 546b5384c61aa1d6e7d720f08591a035d4984fc3
+
