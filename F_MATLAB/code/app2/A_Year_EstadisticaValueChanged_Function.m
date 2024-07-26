bc98befb16f3400c9497b5e8c6d917589515df84
Francis <fayamamani@unsa.edu.pe>
2024-07-25 19:04:41 -0500

    Parte Grafica :: Grafica Recibo

---
+++ F_MATLAB/code/app2/A_Year_EstadisticaValueChanged_Function.m
@@ -0,0 +1,14 @@
+function A_Year_EstadisticaValueChanged_Function(app)
+    sel = app.T_Recibo_1.Data;
+    app.A_Year_Estadistica.Items = (string(unique(app.T_Recibo_1.Data.RecA)))';
+    value = app.A_Year_Estadistica.Value;
+    sel  = sel(sel.RecA == str2double(value),:);
+    sel = sortrows(sel,'RecM','ascend');
+    consumo = sel.RecConMen;
+    mes = sel.RecM;
+    app.G_Estadistica_Recibo.XTick = unique(mes);
+    xlabel(app.G_Estadistica_Recibo,"Mes del Año");
+    ylabel(app.G_Estadistica_Recibo,"Consumo mensual");
+    %app.G_Estadistica_Recibo.
+    plot(app.G_Estadistica_Recibo,mes,consumo);
+end
--- F_MATLAB/code/app2/I_ReciboImageClicked_Function.m
+++ F_MATLAB/code/app2/I_ReciboImageClicked_Function.m
@@ -1,12 +1,3 @@
 function I_ReciboImageClicked_Function(app)
-    sel = app.T_Recibo_1.Data;
-    sel  = sel(sel.RecA == 2023,:);
-    sel = sortrows(sel,'RecM','descend');
-    consumo = sel.RecConMen;
-    mes = sel.RecM;
-    app.G_Estadistica_Recibo.XTick = unique(mes);
-    xlabel(app.G_Estadistica_Recibo,"Mes del Año");
-    ylabel(app.G_Estadistica_Recibo,"Consumo mensual");
-    %app.G_Estadistica_Recibo.
-    plot(app.G_Estadistica_Recibo,mes,consumo);
+    A_Year_EstadisticaValueChanged_Function(app);
 end
