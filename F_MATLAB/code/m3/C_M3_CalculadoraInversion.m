d19a2450acc682a0691f37f18cc7b0315124013e
Gabriel-Bernedo <132683380+Gabriel-Bernedo@users.noreply.github.com>
2024-07-25 23:21:10 -0500

    Update C_M3_CalculadoraInversion.m

--- F_MATLAB/code/m3/C_M3_CalculadoraInversion.m
+++ F_MATLAB/code/m3/C_M3_CalculadoraInversion.m
@@ -19,11 +19,14 @@ classdef C_M3_CalculadoraInversion
     function x = simular(obj)
       ahorro = zeros(obj.tiempo,1);
       costo = zeros(obj.tiempo,1);
+      limite = zeros(obj.tiempo,1);
       ahorro(1) = 6000; % inversion.costoTotal();
       costo(1) = 0;
+      limite(1) = 3400;
       for i = 2:obj.tiempo
         costo(i) = 5.233 * 12 * ((12 * i + 114) * 0.0027 + 0.4286) + costo(i - 1);
-        ahorro(i) = ahorro(1) - costo(i) + obj.costo_mantenimiento / obj.periodo_mantenimiento; % + inversion.costoReemplazo();
+        ahorro(i) = ahorro(1) - costo(i) + obj.costo_mantenimiento * obj.periodo_mantenimiento; % + inversion.costoReemplazo();
+        limite(i) = 3400;
       end
       x = [ahorro, costo];
     end
