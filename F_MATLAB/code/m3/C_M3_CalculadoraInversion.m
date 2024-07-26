aef1f2eb69b861c11866c6770af8655685cb0c33
Gabriel-Bernedo <132683380+Gabriel-Bernedo@users.noreply.github.com>
2024-07-25 22:44:09 -0500

    Update C_M3_CalculadoraInversion.m

--- F_MATLAB/code/m3/C_M3_CalculadoraInversion.m
+++ F_MATLAB/code/m3/C_M3_CalculadoraInversion.m
@@ -3,6 +3,9 @@ classdef C_M3_CalculadoraInversion
     inversion
     tasa_interes
     tiempo
+    usuario
+    costo_mantenimiento
+    periodo_mantenimiento
   end
   methods
     function obj = C_M3_CalculadoraInversion(inversion, tasa_interes, tiempo)
@@ -11,5 +14,15 @@ classdef C_M3_CalculadoraInversion
       obj.tiempo = tiempo;
     end
 
+    function x = simular(obj, inversion)
+      inversion = zeros(obj.tiempo,1);
+      costo = zeros(obj.tiempo,1);
+      inversion(0) = inversion.costoTotal();
+      costo(0) = 0;
+      for i = 1:obj.tiempo
+        costo(i) = obj.usuario.consumoMensual() * 12 * ((12 * i + 114) * 0.0027 + 0.4286) + costo(i - 1);
+        inversion(i) = inversion(i - 1) - costo(i) + obj.costo_mantenimiento / obj.periodo_mantenimiento;
+      end
+    end
   end
 end
