728430858ee1780b19b9ad9355a321de5eb484ce
UNSA_Frank <fhanccoccalloa@unsa.edu.pe>
2024-07-27 20:12:13 -0500

    cambios Frak

--- F_MATLAB/code/app2/Seleccion_Electrodomestico.m
+++ F_MATLAB/code/app2/Seleccion_Electrodomestico.m
@@ -2,6 +2,10 @@ function Seleccion_Electrodomestico(app)
     drawnow;
     value = app.D_Electrodomesticos_IngresarE_1.Value;
     app.Fi_Nombre_anadir_IngresarE_1.Value = value;
+    % Obtener los datos del electrodoméstico seleccionado
     data = app.M1.Obj_Electrodomesticos(value);
+    disp(data);
     app.Fi_PotenciaEnWatts_IngresarE_1.Value = data.consumo_potencia_base;
+    % Llamar a la nueva función para mostrar la imagen
+    mostrar_Imagen(app, data.ambiente.nombre, data.base_codigo);
 end
