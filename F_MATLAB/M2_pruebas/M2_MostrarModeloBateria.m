10740dd7440dd4488ff5335bf653aeace604ed3c
Jeanpier Valera <jvalera@unsa.edu.pe>
2024-07-28 02:41:25 -0500

    cmabios en m2 preubas, editando M2_mockup

--- F_MATLAB/M2_pruebas/M2_MostrarModeloBateria.m
+++ F_MATLAB/M2_pruebas/M2_MostrarModeloBateria.m
@@ -1,7 +1,51 @@
-function mostrarModelo = M2_MostrarModeloBateria()
+function mostrar = M2_MostrarModeloBateria(tipo)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-mostrarModelo = 'BATERIA ModeloXX';
+    switch tipo
+        case 'modelo'
+            mostrar = mostrarModelo();
+        case 'capacidad'
+            mostrar = mostrarCapacidad();
+        case 'largo'
+            mostrar = mostrarLargo();
+        case 'ancho'
+            mostrar = mostrarAncho();
+        case 'alto'
+            mostrar = mostrarAlto();
+        case 'costo'
+            mostrar = mostrarCosto();
+        case 'tvida'
+            mostrar = mostrarTvida();
+        otherwise
+            error('Tipo no válido.');
+    end
 
 end
 
+function mostrar = mostrarModelo()
+    mostrar = 'Bateria ModeloXX';
+end
+
+function mostrar = mostrarCapacidad()
+    mostrar = 100;
+end
+
+function mostrar = mostrarLargo()
+    mostrar = 110;
+end
+
+function mostrar = mostrarAncho()
+    mostrar = 120;
+end
+
+function mostrar = mostrarAlto()
+    mostrar = 130;
+end
+
+function mostrar = mostrarCosto()
+    mostrar = 140;
+end
+
+function mostrar = mostrarTvida()
+    mostrar = 150;
+end
--- F_MATLAB/M2_pruebas/M2_MostrarModeloInversor.m
+++ F_MATLAB/M2_pruebas/M2_MostrarModeloInversor.m
@@ -1,7 +1,39 @@
-function mostrarModelo = M2_MostrarModeloInversor()
+function mostrar = M2_MostrarModeloInversor(tipo)
 %M2_MOSTRARMODELOINVERSOR Summary of this function goes here
 %   Detailed explanation goes here
-mostrarModelo = 'INVERSOR ModeloXX';
+    switch tipo
+        case 'modelo'
+            mostrar = mostrarModelo();
+        case 'potencia'
+            mostrar = mostrarPotencia();
+        case 'vadmisible'
+            mostrar = mostrarVadmisible();
+        case 'costo'
+            mostrar = mostrarCosto();
+        case 'tvida'
+            mostrar = mostrarTvida();
+        otherwise
+            error('Tipo no válido.');
+    end
 
 end
 
+function mostrar = mostrarModelo()
+    mostrar = 'Inversor ModeloXX';
+end
+
+function mostrar = mostrarPotencia()
+    mostrar = 100;
+end
+
+function mostrar = mostrarVadmisible()
+    mostrar = 110;
+end
+
+function mostrar = mostrarCosto()
+    mostrar = 140;
+end
+
+function mostrar = mostrarTvida()
+    mostrar = 150;
+end
