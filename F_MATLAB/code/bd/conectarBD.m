bbbe1fb1a7e0481380840a46508ec0c49aa716c1
Jeanpier Valera <jvalera@unsa.edu.pe>
2024-07-28 15:05:25 -0500

    code fixed - conectando a la BD - arreglos en conn

--- F_MATLAB/code/bd/conectarBD.m
+++ F_MATLAB/code/bd/conectarBD.m
@@ -1,13 +1,13 @@
 function conn = conectarBD()
     %nombre de tu conexion
-    dbname = 'ConectarBD';
+    dbname = 'mydb';
     %Usuario de tu conexion
     username = 'root';
     %password de tu conexion
-    password = '';
+    password = '1234';
     driver = 'com.mysql.cj.jdbc.Driver';
-    url = 'jdbc:mysql://localhost:3306';
-    conn = database(dbname, username, password);
+    url = 'jdbc:mysql://localhost:3306/mydb';
+    conn = database(dbname, username, password,driver, url);
     if ~isopen(conn)
         error('No se pudo conectar a la base de datos: %s', conn.Message);
     end
