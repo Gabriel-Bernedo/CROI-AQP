d9dbdc5727a9d34225112df4f9ca18abc6007dcd
Jeanpier Valera <jvalera@unsa.edu.pe>
2024-07-28 17:17:39 -0500

    hotfix - conector BD , detalles credenciales personales

--- F_MATLAB/code/bd/conectarBD.m
+++ F_MATLAB/code/bd/conectarBD.m
@@ -1,13 +1,13 @@
 function conn = conectarBD()
-   %nombre de tu conexion
-    dbname = 'mydb';
+    %nombre de tu conexion
+    dbname = 'ConectarBD';
     %Usuario de tu conexion
     username = 'root';
     %password de tu conexion
-    password = '1234';
+    password = '';
     driver = 'com.mysql.cj.jdbc.Driver';
-    url = 'jdbc:mysql://localhost:3306/mydb';
-    conn = database(dbname, username, password,driver, url);
+    url = 'jdbc:mysql://localhost:3306';
+    conn = database(dbname, username, password);
     if ~isopen(conn)
         error('No se pudo conectar a la base de datos: %s', conn.Message);
     end
