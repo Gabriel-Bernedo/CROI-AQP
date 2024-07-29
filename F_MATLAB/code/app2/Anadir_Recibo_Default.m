e003ec22ed56391b9678ef2f4bf443f5dfcadfe4
Francis <fayamamani@unsa.edu.pe>
2024-07-28 22:31:46 -0500

    Alertas __ completas

---
+++ F_MATLAB/code/app2/Anadir_Recibo_Default.m
@@ -0,0 +1,5 @@
+function Anadir_Recibo_Default(app)
+    app.Fi_ConsumoMesA_IngresarR_1.Value = NaT;
+    app.Fi_CostoTotal_IngresarR_1.Value = 0;
+    app.Fi_ConsumoMensual_IngresarR_1.Value = 0;
+end
--- F_MATLAB/code/app2/Update_Recibo.m
+++ F_MATLAB/code/app2/Update_Recibo.m
@@ -7,12 +7,20 @@ function Update_Recibo(app)
     selection = app.T_Recibo_1.Selection;
     sel = app.T_Recibo_1.Data([selection(:,1)], :);
     %(codigo, consumo, costo, Año, Mes)
-    data = {sel{1},consumo,costo, Year, Month}
-    app.M1 = app.M1.E_Recibos(data);
-    Tabla_Recibos(app);
-    app.Fi_ReciboCostoTotal_1.Editable ="off";
-    app.Fi_ReciboConsumo_1.Editable = "off";
-    app.B_EditarRecibo_1.Visible = "on";
-    app.B_EliminarRecibo_1.Visible = "off";
-    app.B_ActualizarRecibo_1.Visible = "off";
+   ques = sprintf('¿Estas seguro de actualizar:\nConsumo: %s\nCosto: %s\nAño: %s\nMes: %s', ...
+               num2str(consumo), num2str(costo), num2str(Year), num2str(Month));
+    opcion = uiconfirm(app.P_Main,ques, ...
+    'Confirmacion', 'Options',{'Si','No'}, 'DefaultOption',2,'CancelOption',2, ...
+    'Icon','warning');
+    if strcmp(opcion,'Si')
+        data = {sel{1},consumo,costo, Year, Month};
+        app.M1 = app.M1.E_Recibos(data);
+        Tabla_Recibos(app);
+        Gestionar_Recibo_Default(app);
+        app.Fi_ReciboCostoTotal_1.Editable ="off";
+        app.Fi_ReciboConsumo_1.Editable = "off";
+        app.B_EditarRecibo_1.Visible = "on";
+        app.B_EliminarRecibo_1.Visible = "off";
+        app.B_ActualizarRecibo_1.Visible = "off";
+    end
 end
